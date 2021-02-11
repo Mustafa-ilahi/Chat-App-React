d195abe7a8caafbd80a6bb8b23a064ff9d494088
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-12 03:06:46 +0500

    some changes

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -5,7 +5,7 @@ import { facebook_login } from '../../store/action';
 
 class Home extends Component{
   render(){
-      console.log("props==>", this.props);
+      // console.log("props==>", this.props);
     return(
       <div>
         <h1>Home</h1>
