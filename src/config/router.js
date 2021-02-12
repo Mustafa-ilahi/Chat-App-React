52e72da3e44c207ca62428dafd055a242f8c9e44
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-13 03:08:23 +0500

    updated

--- src/config/router.js
+++ src/config/router.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "../containers/Home";
-import Chat from "../containers/Chat";
+// import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Home from "../containers/Home";
+// import Chat from "../containers/Chat";
 
-class AppRouter extends Component{
-    render(){
-        return(
-            <Router>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/chat" component={Chat} />
-            </Router>
-        )
-    }
-}
+// class AppRouter extends Component{
+//     render(){
+//         return(
+//             <Router>
+//                 <Route exact path="/" component={Home} />
+//                 <Route exact path="/chat" component={Chat} />
+//             </Router>
+//         )
+//     }
+// }
 
-export default AppRouter;
+// export default AppRouter;
