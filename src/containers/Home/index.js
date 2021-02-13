8f912966b4ee88bd902f502f6778ee31f7a52b05
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-14 02:03:02 +0500

    updated

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,29 +1,29 @@
-// import './style.css';
-// import React, {Component} from 'react';
-// import { connect } from 'react-redux';
-// import { facebook_login } from '../../store/action';
+import './style.css';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { facebook_login } from '../../store/action';
 
-// class Home extends Component{
-//   render(){
+class Home extends Component{
+  render(){
       
-//     return(
-//       <div>
-//         <h1>Home</h1>
-//         <button onClick={()=> this.props.facebook_login(this.props.history)}>Facebook Login</button>
-//       </div>
-//     )
-//   }
-// }
+    return(
+      <div>
+        <h1>Home</h1>
+        <button onClick={()=> this.props.facebook_login(this.props.history)}>Facebook Login</button>
+      </div>
+    )
+  }
+}
 
-// const mapStateToProps = (state) => (
-//     {
-//         name: state.username,
-//         email: state.email
-//   }
-// )
+const mapStateToProps = (state) => (
+    {
+        name: state.username,
+        email: state.email
+  }
+)
 
-// const mapDispatchToProps = (dispatch) => ({
-//     facebook_login: (history) => dispatch(facebook_login(history))
-//   })
+const mapDispatchToProps = (dispatch) => ({
+    facebook_login: (history) => dispatch(facebook_login(history))
+  })
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
