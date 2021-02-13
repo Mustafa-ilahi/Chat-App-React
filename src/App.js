26971bf4f005cc6e48fe83a59286d5d3a7926fd9
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-14 02:03:57 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React, {Component} from 'react';
-// import AppRouter from './config/router';
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+import AppRouter from './config/router';
 
-// class App extends Component{
-//   render(){
-//     return(
-//       <div>
-//         <AppRouter />
-//       </div>
-//     )
-//   }
-// }
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <AppRouter />
+      </div>
+    )
+  }
+}
 
-// export default App;
+export default App;
