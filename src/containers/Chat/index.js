879c6c325b12023e2f0a800d68543c022fcb9ae2
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-15 01:08:01 +0500

    updated

--- src/containers/Chat/index.js
+++ src/containers/Chat/index.js
@@ -5,37 +5,62 @@ import { get_users } from "../../store/action";
 import { bindActionCreators } from 'redux';
 
 class Chat extends Component{
+    constructor(){
+        super()
+        this.state = {
+            chat_user : {},
+            chat : []
+        }
+    }
     componentDidMount(){
         this.props.get_users();
     }
 
-    render(){
+    chat = (user) =>{
+        // console.log(user);
+        this.setState({
+            chat_user : user
+        })
+    }
 
+    render(){
+        // console.log(this.state.chat_user);
         let user = this.props.current_user;
         return(
             <div>
                 <div>
-                    <h2> Welcome {user.name} ..!</h2>
-                    <img src={user.profile} />
-                    <h3>Email: {user.email}</h3>
+                    <h2> <img src={user.profile} /> <span id="head">Welcome {user.name} </span></h2>
                 </div>
                 <div className="mainDiv">
                     <div>
                         <h3>Chat Users:</h3>
                         <ul>
                             {this.props.users.map((v,i)=>{
-                                return v.uid !== user.uid && <li key={i}><img src={v.profile} alt="" width="20" /> {v.name} <button>Chat</button></li> 
+                                return   user.uid !== v.uid && <li key={i}><img src={v.profile} alt="" width="20" /> {v.name} 
+                                <button onClick={()=> this.chat(v)}>Chat</button></li> 
                                 }) }
                         </ul>
                     </div>
                     <div className="chatBox">
                         <h1>Messenger</h1>
+                        {Object.keys(this.state.chat_user ).length ? 
+                        <h4> &nbsp; &nbsp;
+                        <img src={this.state.chat_user.profile} width="20"/> &nbsp; 
+                        {this.state.chat_user.name}
+                            </h4>
+                            :
+                            <h4 style={{textAlign:"center"}}> No user</h4>
+                    }
                         <div className="messageInput">
-                        <input type="text" placeholder="Enter Message"/>
-                        <button className="sendBtn">Send</button>
+                             
+                            <input type="text" placeholder="Enter Message"/>
+                            <button className="sendBtn">Send</button>
                         </div>
+                        
                     </div>
                 </div>
+                <br/>
+                <br/>
             </div>
         )
     }
