b1c839664ba2898a4b43e3d5e12c8383590ecb17
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-16 03:08:24 +0500

    almost completed

--- src/containers/Chat/index.js
+++ src/containers/Chat/index.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './style.css';
 import { connect } from 'react-redux';
 import { get_users } from "../../store/action";
-import { bindActionCreators } from 'redux';
+import firebase from '../../config/firebase';
 
 class Chat extends Component{
     constructor(){
         super()
         this.state = {
             chat_user : {},
-            chat : [],
+            chats : [],
             message : ""
         }
     }
@@ -18,20 +18,33 @@ class Chat extends Component{
     }
 
     chat = (user) =>{
-        // console.log(user);
         this.setState({
             chat_user : user
         })
+        let current_user = this.props.current_user;
+        let merge_uid = this.uid_merge(current_user.uid,user.uid);
+        // console.log("==>",merge_uid)
+        this.get_messages(merge_uid);
     }
 
     send_message = () =>{
-        this.state.chat.push({
-            message:this.state.message
+        let user = this.props.current_user;
+        let chat_user = this.state.chat_user;
+        let merge_uid = this.uid_merge(user.uid,chat_user.uid);
+        firebase.database().ref('/').child(`chats/ ${merge_uid}`).push({
+            message: this.state.message,
+            name: user.name,
+            uid: user.uid
         })
-        this.setState({
-            chat: this.state.chat,
-            message: ""
-        })
+    }
+
+    get_messages = (uid) => {
+        firebase.database().ref('/').child(`chats/${uid}`).on("child_added",(messages)=>{
+            this.state.chats.push(messages.val());
+            this.setState({
+                chats: this.state.chats
+            })
+        })    
     }
 
     uid_merge = (uid1,uid2) =>{
@@ -43,7 +56,7 @@ class Chat extends Component{
         }
     }
     render(){
-        // console.log(this.state.chat_user);
+        console.log(this.props.chat);
         let user = this.props.current_user;
         return(
             <div>
@@ -67,7 +80,7 @@ class Chat extends Component{
                         <img src={this.state.chat_user.profile} width="20"/> &nbsp; 
                         {this.state.chat_user.name}
                         <ul>
-                            {this.state.chat.map((v,i)=>{
+                            {this.state.chats.map((v,i)=>{
                                 return <li key={i}>{v.message}</li>
                             })}
                         </ul>
